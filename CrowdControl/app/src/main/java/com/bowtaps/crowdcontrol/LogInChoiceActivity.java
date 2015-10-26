727a9276dd073915e2d4dd5f2ee8e31d4fbb97d7
Daniel Andrus <danieleandrus@gmail.com>
2015-10-26 09:49:07 -0600

    Added template login activity
    + Added LoginActivity activity
    + Added activity_login layout
    + Clicking either button on the LoginChoiceActivity launches the
    LoginActivity
    * Renamed LogInChoiceActivity => LoginChoiceActivity
    * Renamed activity_log_in_choice => activity_login_choice

--- CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/LogInChoiceActivity.java
+++ CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/LogInChoiceActivity.java
@@ -1,16 +1,30 @@
 package com.bowtaps.crowdcontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class LogInChoiceActivity extends AppCompatActivity {
+public class LoginChoiceActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button mButtonFacebook;
+    Button mButtonCrowdControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in_choice);
+        setContentView(R.layout.activity_login_choice);
+
+        // Get handles to buttons
+        mButtonFacebook = (Button) findViewById(R.id.login_choice_facebook_button);
+        mButtonCrowdControl = (Button) findViewById(R.id.login_choice_crowd_control_button);
+
+        // Declare this activity to handle button clicks
+        mButtonFacebook.setOnClickListener(this);
+        mButtonCrowdControl.setOnClickListener(this);
     }
 
     @Override
@@ -34,4 +48,38 @@ public class LogInChoiceActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+        // Handles clicks onn items in view
+        // in this case, either the facebook button or the create account button
+
+        switch (v.getId()) {
+            case R.id.login_choice_facebook_button:
+                onFacebookButtonClick((Button) v);
+                break;
+
+            case R.id.login_choice_crowd_control_button:
+                onCreateAccountButtonClick((Button) v);
+                break;
+
+            default:
+                // Sorry, you're outta luck
+                break;
+        }
+    }
+
+    private void onFacebookButtonClick(Button v) {
+        launchLoginActivity();
+    }
+
+    private void onCreateAccountButtonClick(Button b) {
+        launchLoginActivity();
+    }
+
+    private void launchLoginActivity() {
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        this.startActivity(myIntent);
+    }
+
 }
