15adc130b452fcd88183e1c9d07605f46e794141
Joseph Mowry <joseph.mowry@mines.sdsmt.edu>
2015-10-28 21:02:51 -0600

    GroupModel class and interface written
    Finished GroupModel and its corresponding interface, including getters
    and setters

--- CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/model/GroupModel.java
+++ CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/model/GroupModel.java
@@ -1,34 +1,18 @@
 package com.bowtaps.crowdcontrol.model;
 
-import java.security.acl.Group;
-
 /**
- * Created by 1959760 on 10/24/2015.
+ * Created by 1959760 on 10/28/2015.
  */
-public class GroupModel {
+public interface GroupModel {
+    // Get Methods
+    public String getGroupName();
+    public Object getItinerary();
+    public String getStatus();
+    public Object getWaypoints();
 
-    public GroupModel()
-    {
-        // Initialize Fields
-        // TODO: user GroupLeader;
-        GroupName = null;
-        Itinerary = null;
-        Status = null;
-        Waypoints = null;
-        // TODO: List of users in the group
-
-    }
-    // Fields
-    public String GroupName;
-    public Object Itinerary;
-    public String Status;
-    public Object Waypoints;
-
-
-    // Methods
-//        GetGroupModel();
-//        GetGroupName();
-//        GetItinerary();
-//        GetStatus();
-//        GetWaypoints();
+    // Set Methods
+    public void setGroupName(String groupName);
+    public void setItinerary(Object itinerary);
+    public void setStatus(String status);
+    public void setWaypoints(Object waypoints);
 }
