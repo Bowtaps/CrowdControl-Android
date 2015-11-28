2bb01b3647e4111ef7bb0e8fd667b6c851d44f71
Johnathan Ackerman <johnny.ackerman@bowtaps.com>
2015-11-28 16:16:39 -0700

    couple fixes after merge
    thought i got them during the merge

--- CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/controllers/LoginController.java
+++ CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/controllers/LoginController.java
@@ -2,10 +2,12 @@ package com.bowtaps.crowdcontrol.controllers;
 
 import android.content.Context;
 
+import com.bowtaps.crowdcontrol.model.BaseModel;
 import com.bowtaps.crowdcontrol.model.GroupModel;
 import com.bowtaps.crowdcontrol.model.UserModel;
 
 import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Created by 1959760 on 11/12/2015.
@@ -16,6 +18,51 @@ public class LoginController {
 
     public LoginController(Context app_context){
         model = new UserModel() {
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public Date getCreated() {
+                return null;
+            }
+
+            @Override
+            public Date getUpdated() {
+                return null;
+            }
+
+            @Override
+            public Boolean wasModified() {
+                return null;
+            }
+
+            @Override
+            public void save() throws Exception {
+
+            }
+
+            @Override
+            public void saveInBackground(SaveCallback callback) {
+
+            }
+
+            @Override
+            public void load() throws Exception {
+
+            }
+
+            @Override
+            public void loadInBackground(LoadCallback callback) {
+
+            }
+
+            @Override
+            public String getUsername() {
+                return null;
+            }
+
             @Override
             public String getObjectID() {
                 return null;
@@ -42,82 +89,17 @@ public class LoginController {
             }
 
             @Override
-            public DateFormat getCreatedAt() {
-                return null;
-            }
-
-            @Override
-            public DateFormat getUpdatedAt() {
-                return null;
-            }
-
-            @Override
-            public Object getLocation() {
-                return null;
-            }
-
-            @Override
-            public Object getPreferences() {
-                return null;
-            }
-
-            @Override
-            public String getStatus() {
-                return null;
-            }
-
-            @Override
-            public void setObjectID(String objectID) {
-
-            }
-
-            @Override
-            public void setUserName(String userName) {
-
-            }
-
-            @Override
-            public void setPassword(String password) {
-
-            }
-
-            @Override
-            public void setAuthData(Object authData) {
-
-            }
-
-            @Override
-            public void setEmailVerified(Boolean isVerified) {
-
-            }
-
-            @Override
             public void setEmail(String email) {
 
             }
 
             @Override
-            public void setCreatedAt(DateFormat createdDate) {
-
+            public String getPhone() {
+                return null;
             }
 
             @Override
-            public void setUpdatedAt(DateFormat updatedDate) {
-
-            }
-
-            @Override
-            public void setLocation(Object location) {
-
-            }
-
-            @Override
-            public void setPreferences(Object preferences) {
-
-            }
-
-            @Override
-            public void setStatus(String status) {
+            public void setPhone(String phone) {
 
             }
 
