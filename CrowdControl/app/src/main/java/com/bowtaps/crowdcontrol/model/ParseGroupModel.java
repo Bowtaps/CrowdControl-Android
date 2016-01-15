d7aa3ad3b69ca5ef7d561e533e80f39379958d0e
Joseph Mowry <joseph.mowry@mines.sdsmt.edu>
2016-01-15 12:53:08 -0700

    Setup GroupInfoFragment to handle button clicks on the Leave Group button.
    Not complete yet.

--- CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/model/ParseGroupModel.java
+++ CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/model/ParseGroupModel.java
@@ -109,4 +109,6 @@ public class ParseGroupModel extends ParseBaseModel implements GroupModel{
         ParseRelation relation = CrowdControlApplication.aGroup.getRelation("GroupMembers");
         relation.add( userProfile );
     }
+
+
 }
