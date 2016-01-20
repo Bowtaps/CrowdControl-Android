44c16bcbbc8cbcac6fafbe7179d8b3862c7e867d
Hamm3rTim3 <evan.hammer@mines.sdsmt.edu>
2016-01-19 20:08:47 -0700

    Did it work?

---
+++ CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/model/LocationModel.java
@@ -0,0 +1,66 @@
+package com.bowtaps.crowdcontrol.model;
+
+/**
+ * Created by Evan Hammer on 1/19/16.
+ */
+public interface LocationModel extends BaseModel {
+
+    /**
+     * Gets the latitude variable, converts it from a string to a double and
+     *  returns it.
+     *
+     * @return latitude as a double.
+     */
+    public double getLatitude();
+
+    /**
+     * Gets the longitude variable, converts it from a string to a double
+     * and returns it.
+     *
+     * @return longitude as a double.
+     */
+    public double getLongitude();
+
+    /**
+     * Gets the To field and returns the UserProfileModel.
+     *
+     * @return UserProfileModel containing the user who is receiving the
+     *          location.
+     */
+    public UserProfileModel getTo();
+
+    /**
+     * Gets the From field and returns the UserProfileModel.
+     *
+     * @return UserProfileModel containing the user who sent the location.
+     */
+    public UserProfileModel getFrom();
+
+    /**
+     * Sets the Latitude variable, converting from a double to a string.
+     *
+     * @param latitude - value for the latitudinal coordinate.
+     */
+    public void setLatitude(double latitude);
+
+    /**
+     * Sets the Longitude variable, converting from a double to a string.
+     *
+     * @param longitude - value for the longitudinal coordinate.
+     */
+    public void setLongitude(double longitude);
+
+    /**
+     * Sets the To field, the user Receiving the location
+     *
+     * @param userProfileModel - UserProfileModel of the location recipient
+     */
+    public void setTo(UserProfileModel userProfileModel);
+
+    /**
+     * Sets the From field, the user that sent the location.
+     *
+     * @param userProfileModel - UserProfileModel of the location sender
+     */
+    public void setFrom(UserProfileModel userProfileModel);
+}
---
+++ CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/model/ParseLocationModel.java
@@ -0,0 +1,126 @@
+package com.bowtaps.crowdcontrol.model;
+
+import com.parse.ParseObject;
+
+/**
+ * Created by Evan Hammer on 1/19/16.
+ */
+public class ParseLocationModel extends ParseBaseModel implements LocationModel{
+    /**
+     * The name of the table that this object is designed to interact with.
+     */
+    private static final String tableName = "Location";
+
+    /**
+     * Key corresponding to {@link ParseLocationModel#getTo()}
+     */
+    private static final String toKey = "To";
+
+    /**
+     * Key corresponding to {@link ParseLocationModel#getFrom()}
+     */
+    private static final String fromKey = "From";
+
+    /**
+     * Key corresponding to {@link ParseLocationModel#getLatitude()}
+     */
+    private static final String latitudeKey = "Latitude";
+
+    /**
+     * Key corresponding to {@link ParseLocationModel#getLongitude()}
+     */
+    private static final String longitudeKey = "Longitude";
+
+    /**
+     * The class constructor. Initializes the model from an existing
+     * {@link ParseLocationModel}.
+     *
+     * @param object The object to use as a handle.
+     */
+    public ParseLocationModel(ParseObject object) {
+        super(object);
+    }
+
+    /**
+     * Gets the latitude variable, converts it from a string to a double and
+     *  returns it.
+     *
+     * @return latitude as a double.
+     */
+    @Override
+    public double getLatitude(){
+        return (double) latitude;
+    }
+
+    /**
+     * Gets the longitude variable, converts it from a string to a double
+     * and returns it.
+     *
+     * @return longitude as a double.
+     */
+    @Override
+    public double getLongitude(){
+        return (double) ((ParseObject) parseObject).get(longitudeKey);
+    }
+
+    /**
+     * Gets the To field and returns the UserProfileModel.
+     *
+     * @return UserProfileModel containing the user who is receiving the
+     *          location.
+     */
+    @Override
+    public UserProfileModel getTo(){
+        return (UserProfileModel) ((ParseObject) parseObject).get(toKey);
+    }
+
+    /**
+     * Gets the From field and returns the UserProfileModel.
+     *
+     * @return UserProfileModel containing the user who sent the location.
+     */
+    @Override
+    public UserProfileModel getFrom(){
+        return (UserProfileModel) ((ParseObject) parseObject).get(fromKey);
+    }
+
+    /**
+     * Sets the Latitude variable, converting from a double to a string.
+     *
+     * @param latitude - value for the latitudinal coordinate.
+     */
+    @Override
+    public void setLatitude(double latitude){
+        parseObject.put(latitudeKey, latitude);
+    }
+
+    /**
+     * Sets the Longitude variable, converting from a double to a string.
+     *
+     * @param longitude - value for the longitudinal coordinate.
+     */
+    @Override
+    public void setLongitude(double longitude){
+        parseObject.put(longitudeKey, longitude);
+    }
+
+    /**
+     * Sets the To field, the user Receiving the location
+     *
+     * @param userProfileModel - UserProfileModel of the location recipient
+     */
+    @Override
+    public void setTo(UserProfileModel userProfileModel){
+        //set the user profile
+    }
+
+    /**
+     * Sets the From field, the user that sent the location.
+     *
+     * @param userProfileModel - UserProfileModel of the location sender
+     */
+    @Override
+    public void setFrom(UserProfileModel userProfileModel){
+        //set the user profile
+    }
+}
