70687c2197177786f0822a95bad1d8b0cbb1c3fb
Johnathan Ackerman <johnny.ackerman@bowtaps.com>
2016-03-27 16:41:34 -0600

    logging out is now the fastest way to leave a group

--- CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/GroupInfoFragment.java
+++ CrowdControl/app/src/main/java/com/bowtaps/crowdcontrol/GroupInfoFragment.java
@@ -183,7 +183,10 @@ public class GroupInfoFragment extends Fragment implements View.OnClickListener,
         }
         else {
             CrowdControlApplication.getInstance().getModelManager().setCurrentGroup(null);
-            launchGroupJoinActivity();
+            //race conditions can cause this to happen during a log out sequence, check to make sure not logged out
+            if (CrowdControlApplication.getInstance().getModelManager().getCurrentUser() != null) {
+                launchGroupJoinActivity();
+            }
         }
     }
 
@@ -207,11 +210,11 @@ public class GroupInfoFragment extends Fragment implements View.OnClickListener,
      * @param leaveMessage
      */
     public void leaveDialog(Activity activity, String title, CharSequence message, String leaveMessage){
-        AlertDialog.Builder buildDialog = new AlertDialog.Builder(activity);
+        AlertDialog.Builder buildThis = new AlertDialog.Builder(activity);
 
-        buildDialog.setTitle(title);
-        buildDialog.setMessage(message);
-        buildDialog.setPositiveButton(leaveMessage, new DialogInterface.OnClickListener() {
+        buildThis.setTitle(title);
+        buildThis.setMessage(message);
+        buildThis.setPositiveButton(leaveMessage, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 mGroup.removeGroupMember(CrowdControlApplication.getInstance().getModelManager().getCurrentUser().getProfile());
 
@@ -231,8 +234,8 @@ public class GroupInfoFragment extends Fragment implements View.OnClickListener,
                 });
             }
         });
-        buildDialog.setNegativeButton("No go back", null);
-        buildDialog.show();
+        buildThis.setNegativeButton("No go back", null);
+        buildThis.show();
     }
 
     /**
